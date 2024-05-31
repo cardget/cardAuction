9f77ab111695a54a298f269bcdebcb83de704ad3
cocopg <jsjpati@naver.com>
2024-05-31 17:50:46 +0900

    front메인, 헤더, 푸터

---
+++ src/front/js/main.js
@@ -0,0 +1,22 @@
+//header, footer 불러오기
+async function asyncMarkupData() {
+    const allElements = document.getElementsByTagName("*");
+    Array.prototype.forEach.call(allElements, function (el) {
+      const includePath = el.dataset.includePath;
+      if (includePath) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            el.outerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", includePath, true);
+        xhttp.send();
+      }
+    });
+  }
+  
+  asyncMarkupData()
+  .then(() => {
+    return loadItems();
+  });
