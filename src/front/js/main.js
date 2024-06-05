a3c8d863f36428d40a9c410a80323f806f54a735
cocopg <jsjpati@naver.com>
2024-06-05 16:29:06 +0900

    카드효과 css, html, js추가 정성진 커밋

--- src/front/js/main.js
+++ src/front/js/main.js
@@ -1,21 +1,43 @@
 //header, footer 불러오기
 async function asyncMarkupData() {
-    const allElements = document.getElementsByTagName("*");
-    Array.prototype.forEach.call(allElements, function (el) {
-      const includePath = el.dataset.includePath;
-      if (includePath) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            el.outerHTML = this.responseText;
-          }
-        };
-        xhttp.open("GET", includePath, true);
-        xhttp.send();
-      }
-    });
-  }
-  asyncMarkupData()
+  const allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    const includePath = el.dataset.includePath;
+    if (includePath) {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
+  });
+}
+asyncMarkupData()
   .then(() => {
     return loadItems();
   });
+  
+/*카드 움직이기 */
+var container = document.querySelector('.container')
+var overlay = document.querySelector('.overlay')
+container.addEventListener('mousemove', function (e) {
+  var x = e.offsetX
+  var y = e.offsetY
+  console.log(x, y);
+  var rotateY = -1 / 5 * x + 20
+  var rotateX = 4 / 30 * y - 20
+
+  overlay.style = `background-position : ${x / 5 + y / 5}%; filter : opacity(${x / 200}) brightness(1.2)`
+
+  container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+})
+
+container.addEventListener('mouseout', function () {
+  overlay.style = 'filter : opacity(0)'
+  container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+})
+
+
