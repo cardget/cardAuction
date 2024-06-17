80efe7456b81c9051fd32cfe131936870b522f0b
YangxGeon <tmdrjs1117@naver.com>
2024-06-17 12:46:18 +0900

    원피스, 디지몬, 유희왕, 스포츠, 채팅방 DTO 생성 DeckSourceDTO, PokemonDTO 수정

--- src/main/java/com/cardproject/myapp/dto/PokemonDTO.java
+++ src/main/java/com/cardproject/myapp/dto/PokemonDTO.java
@@ -1,14 +1,13 @@
 package com.cardproject.myapp.dto;
 
-import java.sql.Date;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter@Setter
+@Getter
+@Setter
 @ToString
-public class Pokemon {
+public class PokemonDTO {
 	private String cardId;
 	private String cardName;
 	private String cardSort;
