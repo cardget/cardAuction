84e60ea6c74b4f959ccae35c9703a2ab50191664
cocopg <jsjpati@naver.com>
2024-06-18 17:38:19 +0900

    feat : 덱만들기 카드 DB조회 추가, oracleDB properties

--- src/main/java/com/cardproject/myapp/controller/DeckMakerController.java
+++ src/main/java/com/cardproject/myapp/controller/DeckMakerController.java
@@ -1,8 +1,19 @@
 package com.cardproject.myapp.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cardproject.myapp.dao.DeckMakerDAO;
+import com.cardproject.myapp.dto.DigimonDTO;
+import com.cardproject.myapp.dto.OnepieceDTO;
+import com.cardproject.myapp.dto.PokemonDTO;
+import com.cardproject.myapp.dto.YugiohDTO;
+import com.cardproject.myapp.service.DeckMakerService;
 
 
 
@@ -21,10 +32,34 @@ public class DeckMakerController {
 		System.out.println("deckListMain page");
 		
 	}
-	@GetMapping("/deckMaker.do")
-	public void auctionInsert() {
-		System.out.println("deckMaker page");
-		
-
-	}
+	@Autowired
+    private DeckMakerService deckMakerService;	
+	
+	@RequestMapping("/pokemonDeckMaker.do")
+    public void pokemonDeckMaker(Model model) {
+        List<PokemonDTO> pCardList = deckMakerService.selectAllPCard();
+        System.out.println(pCardList.size());
+        model.addAttribute("pCardList", pCardList);
+    }
+	
+	@RequestMapping("/yugiohDeckMaker.do")
+    public void yugiohDeckMaker(Model model) {
+        List<YugiohDTO> yCardList = deckMakerService.selectAllYCard();
+        System.out.println(yCardList.size());
+        model.addAttribute("yCardList", yCardList);
+    }
+	
+	@RequestMapping("/digimonDeckMaker.do")
+    public void digimonDeckMaker(Model model) {
+        List<DigimonDTO> dCardList = deckMakerService.selectAllDCard();
+        System.out.println(dCardList.size());
+        model.addAttribute("dCardList", dCardList);
+    }
+	
+	@RequestMapping("/onepieceDeckMaker.do")
+    public void onepieceDeckMaker(Model model) {
+        List<OnepieceDTO> oCardList = deckMakerService.selectAllOCard();
+        System.out.println(oCardList.size());
+        model.addAttribute("oCardList", oCardList);
+    }
 }
