235a5c37662aff6cf327b6a2a48219b8d871fff5
Junsung <hana3253178@gmail.com>
2024-06-27 17:06:36 +0900

    feat: 결제창 만드는 중(미완성)

---
+++ src/main/java/com/cardproject/myapp/dao/PaymentDAO.java
@@ -0,0 +1,24 @@
+package com.cardproject.myapp.dao;
+
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cardproject.myapp.dto.TradeDTO;
+
+
+@Repository
+public class PaymentDAO{
+
+	@Autowired
+	SqlSession sqlSession;
+
+	String namespace = "com.cardproject.myapp.dao";
+
+	// 거래 조회
+	public TradeDTO selectTradeById(int tradeId) {
+        return sqlSession.selectOne(namespace + ".selectUserById", tradeId);
+    }
+}
